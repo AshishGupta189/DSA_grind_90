16c5445a72ee48e4aaac9663564d608f448663bc
Ashish Gupta <askd442@gmail.com>
2025-09-04 17:18:03 +0530

    sep-4 xor and some ques revised

---
+++ leetcode/maxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int sum=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                sum=0;
+            }else{
+                sum++;
+                if(sum>max) max=sum;
+            }
+        }
+        return max;
+    }
+}
---
+++ leetcode/missingNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int[] res=new int[nums.length+1];
+        for(int i:nums){
+            res[i]=1;
+        }
+        for(int i=0;i<res.length;i++){
+            if(res[i]==0){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
