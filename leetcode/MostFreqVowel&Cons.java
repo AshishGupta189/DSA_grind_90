6e33496c8457813c70c7d373ba62817a66909dbc
Ashish Gupta <askd442@gmail.com>
2025-09-13 21:55:13 +0530

    sep:13 potd

---
+++ leetcode/MostFreqVowel&Cons.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        int maxV=0;
+        int maxC=0;
+        Map<Character,Integer> vowel = new HashMap();
+        Map<Character,Integer> conso= new HashMap();
+        for(char c:s.toCharArray()){
+            if(c=='a'||c=='i'||c=='e'||c=='o'||c=='u'){
+                vowel.put(c,vowel.getOrDefault(c,0)+1);
+                if(vowel.get(c)>maxV) maxV=vowel.get(c);
+            }else{
+                conso.put(c,conso.getOrDefault(c,0)+1);
+                if(conso.get(c)>maxC) maxC=conso.get(c);
+            }
+        }
+        return maxC+maxV;
+    }
+}
