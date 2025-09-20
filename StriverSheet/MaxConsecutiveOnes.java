2163146d09c836803abeb0ef52b21472ced3f176
Ashish Gupta <askd442@gmail.com>
2025-09-20 21:14:15 +0530

    sep:20 dsa array stsheet easy

---
+++ StriverSheet/MaxConsecutiveOnes.java
@@ -0,0 +1,17 @@
+package DSA_grind_90.StriverSheet;
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int sum=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                sum=0;
+            }else{
+                sum++;
+                if(sum>max) max=sum;
+            }
+        }
+        return max;
+    }
+}
